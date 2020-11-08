234715d48736e3fc6cd9338a2f062b0f14b87bce
Md Alam <mdalam7246@gmail.com>
2020-11-07 21:37:13 -0800

    All Model Added

---
+++ app/Http/Controllers/DrugAdviceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DrugAdvice;
+use Illuminate\Http\Request;
+
+class DrugAdviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DrugAdvice  $drugAdvice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DrugAdvice $drugAdvice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DrugAdvice  $drugAdvice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DrugAdvice $drugAdvice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DrugAdvice  $drugAdvice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DrugAdvice $drugAdvice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DrugAdvice  $drugAdvice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DrugAdvice $drugAdvice)
+    {
+        //
+    }
+}
